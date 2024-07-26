aa47ae4fe62fabdd66297be28d188034ef00179f
rockr01434 <rocke0882@gmail.com>
2024-07-26 19:37:36 +0500

    Add files via upload

---
+++ DeleteWebsite.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <domain>"
+    exit 1
+fi
+
+DOMAIN=$1
+DOMAIN_BASE=$(echo "$DOMAIN" | cut -d. -f1)
+DOC_ROOT="/home/$DOMAIN_BASE/public_html"
+HTTP_CONFIG_FILE="/etc/httpd/conf.d/$DOMAIN.conf"
+HTTPS_CONFIG_FILE="/etc/httpd/conf.d/${DOMAIN}_ssl.conf"
+LOG_DIR="/var/log/httpd/$DOMAIN"
+PHP_FPM_POOL_FILE="/etc/php-fpm.d/$DOMAIN_BASE.conf"
+USER=$DOMAIN_BASE
+
+# Stop Apache and PHP-FPM services
+systemctl stop httpd
+systemctl stop php-fpm
+
+# Remove Apache configuration files
+rm -f "$HTTP_CONFIG_FILE"
+rm -f "$HTTPS_CONFIG_FILE"
+
+# Remove PHP-FPM pool configuration file
+rm -f "$PHP_FPM_POOL_FILE"
+
+# Remove log directory
+rm -rf "$LOG_DIR"
+
+# Remove the document root directory
+rm -rf "$DOC_ROOT"
+rm -rf "/home/$DOMAIN_BASE"
+
+# Remove SELinux contexts
+semanage fcontext -d "/home/$DOMAIN_BASE/public_html(/.*)?"
+semanage fcontext -d "/var/log/httpd/$DOMAIN(/.*)?"
+
+# Remove the system user
+userdel -r $USER
+
+# Start Apache and PHP-FPM services
+systemctl start httpd
+systemctl start php-fpm
+
+echo "Website: $DOMAIN has been deleted."

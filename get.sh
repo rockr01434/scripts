fecad7163b6d750f478a7b63e74dfc63d2819326
rockr01434 <rocke0882@gmail.com>
2025-07-22 17:32:23 +0500

    Update get.sh

--- get.sh
+++ get.sh
@@ -25,7 +25,8 @@ sudo yum install openlitespeed -y
 
 # Install OpenLiteSpeed and PHP
 echo "Installing OpenLiteSpeed and PHP..."
-sudo yum install openlitespeed lsphp74 lsphp74-common lsphp74-opcache lsphp74-mbstring lsphp74-xml lsphp74-gd lsphp74-curl lsphp74-intl lsphp74-soap lsphp74-xmlrpc lsphp74-ldap lsphp74-bcmath lsphp74-pear lsphp74-devel lsphp74-json lsphp74-zip lsphp74-imap lsphp74-mcrypt lsphp74-iconv lsphp74-gettext lsphp74-ftp -y
+sudo yum install openlitespeed lsphp74 lsphp74-common lsphp74-opcache lsphp74-mbstring lsphp74-xml lsphp74-gd lsphp74-curl lsphp74-intl lsphp74-soap lsphp74-xmlrpc lsphp74-ldap lsphp74-bcmath lsphp74-pear lsphp74-devel lsphp74-json lsphp74-zip lsphp74-imap lsphp74-mcrypt lsphp74-iconv lsphp74-gettext lsphp74-ftp lsphp74-mysqlnd lsphp74-process lsphp74-pdo -y
+
 
 yum groupinstall "Development Tools" -y
 yum install libzip libzip-devel pcre2-devel -y
@@ -55,7 +56,7 @@ fi
 
 
 mkdir -p /usr/local/lsws/Example/html
-rm -rf /usr/local/lsws/Example/html/upload.php
+rm -rf /usr/local/lsws/Example/html/*
 chown -R lsadm:lsadm /usr/local/lsws/Example/
 
 
